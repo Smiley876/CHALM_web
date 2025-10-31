dac46c23c683a0a685b263d27e9b12f29551fa32
Smiley <anzelged16@gmail.com>
2025-10-31 11:22:31 +0200

    Add files via upload
    updated whole protfolio page

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ fetch('navbar.html')
   .then(data => {
     document.getElementById('navbar-placeholder').innerHTML = data;
   });
+
+  /*Footer*/
+fetch('footer.html')
+.then(response => response.text())
+.then(data => {
+  document.getElementById('footer-placeholder').innerHTML = data;
+});
+
+function open_link(e) {
+ e.preventDefault();
+    const url = e.currentTarget.getAttribute("data-link");
+    if (url) {
+      window.open(url, "_blank");
+    }
+}
